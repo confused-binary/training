00daa66a3828c054adf151ec8ea97c2318c19770
confused-binary <confused-binary>
2024-02-18 16:48:25 -0600

    finish adjusting for project testing

--- golang/006_workerpools/main.go
+++ golang/006_workerpools/main.go
@@ -2,45 +2,36 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
 	"time"
 )
 
-func worker(profile string, command <-chan string, results chan<- string) {
-	// for j := range jobs {
-	// cmd := strings.Join(command, " ")
-	cmd := command
-	fmt.Println("Profile", profile, "started job -", cmd)
-	time.Sleep(time.Second)
-	fmt.Println("Profile", profile, "finished job -", cmd)
-	results <- "done!"
-	// }
+func worker(profile string, command string, results chan<- string) {
+	fmt.Println("aws --profile", profile, command)
+	n := rand.Intn((10-3)+1) + 3
+	fmt.Printf("%s sleeping %d seconds...\n", profile, n)
+	time.Sleep(time.Duration(n) * time.Second)
+	results <- profile + " done!"
 }
 
 func main() {
 	profiles := []string{"profile1", "profile2"}
 	argCommand := strings.Join(os.Args[1:], " ")
+
 	command := make(chan string, len(profiles))
 	results := make(chan string, len(profiles))
 
 	for _, profile := range profiles {
-		go worker(profile, command, results)
+		go worker(profile, argCommand, results)
 	}
 
 	command <- argCommand
 	close(command)
 
 	for i := 1; i <= len(profiles); i++ {
-		<-results
+		x := <-results
+		fmt.Println(x)
 	}
-
-	// for j := 1; j <= numJobs; j++ {
-	// 	jobs <- j
-	// }
-	// close(jobs)
-
-	// for a := 1; a <= numJobs; a++ {
-	// 	<-results
-	// }
 }
